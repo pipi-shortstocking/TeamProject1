64a4e5fc388f035bbf95703de3b791ac2423557c
cowha <cowhappy721@naver.com>
2022-05-19 11:58:58 +0900

    sohee

--- mainform1.cs
+++ mainform1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Teamproject1
 {
@@ -15,6 +16,39 @@ namespace Teamproject1
         public mainform1()
         {
             InitializeComponent();
+        }
+
+        private void gobutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            graghform2 graphform = new graghform2();
+            graphform.Show();
+
+        }
+
+        private void loginbutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            loginform2 loginform1 = new loginform2();
+            loginform1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            profileform profileform1 = new profileform();
+            profileform1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            showfashionform3 showfashionform1 = new showfashionform3();
+            showfashionform1.Show();
         }
     }
 }
